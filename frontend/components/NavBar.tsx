e1ca3619f3f9c6725e1ce903204bf0b7bfc95ec0
Stefan Ganswint <stefan.ganswint@mni.thm.de>
2024-05-28 12:46:43 +0200

    changes so the welcome block is shown

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -35,9 +35,9 @@ const NavBar = ({headingVal}: {headingVal: string}) => {
                 </Link>
             </div>
             <div className='flex w-full'>
-                <nav className="flex justify-between space-x-4 border-b px-4 w-full h-[var(--spacing-16,4rem)]">
+                <nav className="flex justify-between space-x-4 border-b px-4 w-full h-[var(--spacing-16,4rem)] pl-64">
                     <div className='flex justify-center items-center bg-red-500 border border-blue-500'>
-                        <svg className='hidden sm:block' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" >
+                        <svg className='hidden md:block' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" >
                             <path d="M19.2543 12.0043C19.2538 16.0083 16.0074 19.2538 12.0033 19.2533C7.99925 19.2528 4.75376 16.0064 4.7543 12.0023C4.75484 7.99825 8.00121 4.75276 12.0053 4.7533C16.0093 4.75384 19.2548 8.00022 19.2543 12.0043Z" stroke="#3F3F50" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M16.2543 12.0039C16.254 14.3511 14.3509 16.2536 12.0037 16.2533C9.65651 16.253 7.75398 14.3499 7.7543 12.0027C7.75462 9.65552 9.65766 7.75298 12.0049 7.7533C14.3521 7.75362 16.2546 9.65667 16.2543 12.0039Z" stroke="#3F3F50" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M13.2543 12.0035C13.2542 12.6938 12.6945 13.2534 12.0041 13.2533C11.3138 13.2532 10.7542 12.6935 10.7543 12.0031C10.7544 11.3128 11.3141 10.7532 12.0045 10.7533C12.6948 10.7534 13.2544 11.3131 13.2543 12.0035Z" stroke="#3F3F50" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
